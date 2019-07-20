ed18962aecc34e606e74c3a9858276c204929088
Trentin Gillis <craishot@gmail.com>
2019-07-20 13:29:35 -0600

    tour-of-heroes: Finish part 2

--- angular-tour-of-heroes/src/app/app.module.ts
+++ angular-tour-of-heroes/src/app/app.module.ts
@@ -1,11 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from "@angular/forms";
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 
 // Angular needs to know how the pieces of our app fit together and what other files and libraries the app requires <- this information is called metadata
+// Some of the metadata is in the @Component decorators that we added to our component class. Other critical metadata is in the @NgModule decorators
+// ^ The most important @NgModule decorator annotates the top level AppModule class
+
+// Every component MUST be declared in EXACTLY ONE NgModule
 
 @NgModule({
   declarations: [
--- angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { Hero } from "../hero";
+import { HEROES } from '../mock-heros';
+import { Hero } from '../hero';
 
 @Component({
   // The selector matches the name of the HTML element that identifies this component within a parent component's template
@@ -8,15 +9,17 @@ import { Hero } from "../hero";
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  hero: Hero = {
-    id: 1,
-    name: "Windstorm"
-  };
+  heroes = HEROES;
+  selectedHero: Hero;
 
   constructor() { }
 
   // Angular calls ngOnInit shortly after the component is created
   // ^ This is a good place to put initialization logic
   ngOnInit() {/**/}
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 
 }
