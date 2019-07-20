978f6e5cd64e3febbe98c67981cf5b57ca4d969f
Trentin Gillis <craishot@gmail.com>
2019-07-20 13:37:31 -0600

    tour-of-heroes: Finish part 3

--- angular-tour-of-heroes/src/app/app.module.ts
+++ angular-tour-of-heroes/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 
 // Angular needs to know how the pieces of our app fit together and what other files and libraries the app requires <- this information is called metadata
 // Some of the metadata is in the @Component decorators that we added to our component class. Other critical metadata is in the @NgModule decorators
@@ -14,7 +15,8 @@ import { HeroesComponent } from './heroes/heroes.component';
 @NgModule({
   declarations: [
     AppComponent,
-    HeroesComponent
+    HeroesComponent,
+    HeroDetailComponent
   ],
   imports: [
     BrowserModule,
