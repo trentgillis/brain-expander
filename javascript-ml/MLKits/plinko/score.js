52a7fde9c2a039b0918e732065b73a2e0534021d
Trentin Gillis <craishot@gmail.com>
2020-04-24 16:46:46 -0600

    javascript-ml: Complete Section 1 - What is Machine Learning

---
+++ javascript-ml/MLKits/plinko/score.js
@@ -0,0 +1,9 @@
+const outputs = [];
+
+function onScoreUpdate(dropPosition, bounciness, size, bucketLabel) {
+  outputs.push([dropPosition, bounciness, size, bucketLabel]);
+}
+
+function runAnalysis() {
+  // Write code here to analyze stuff
+}
